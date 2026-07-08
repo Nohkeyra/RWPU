c4c778ca57d9f430a15facaeed4a09d21977b32a
Nohkeyra <122115637+Nohkeyra@users.noreply.github.com>
2026-07-08 18:12:17 +0800

    chore: update branding and company list
    Rename application to "Restoran Wawasan" and update company address details.

--- src/constants/companies.ts
+++ src/constants/companies.ts
@@ -9,5 +9,5 @@ export const SAVED_COMPANIES = [
   'KLCC (Holdings) Sdn Bhd, Group Financial Services (GFS), Level 9, Menara Permata Sapura, Kuala Lumpur City Centre',
   'KLCC Property Holdings Sdn Bhd, Level 33 & 34, Menara Dayabumi, Jalan Sultan Hishamuddin, Kuala Lumpur',
   'KLCC Projects Services Sdn Bhd, Level 32, Kompleks Dayabumi, Jalan Sultan Hishamuddin, Kuala Lumpur',
-  'KLCC Projeks Services Sdn Bhd',
+  'KLCC Projeks Sdn Bhd, Level 4, Menara PjH, No 2 Jalan Tun Razak, Presint 2, 62100 Putrajaya',
 ];
